cbb85613f3da3044012129b64898329ccda4f153
vijay-ps <vijay.ps2022@vitstudent.ac.in>
2023-05-02 20:59:06 +0530

    added index.html

---
+++ script.js
@@ -0,0 +1,25 @@
+const btn = document.getElementById("btn");
+const dob = document.getElementById("dob");
+const result = document.getElementById("result");
+
+function calculateAge() {
+  const bvalue = dob.value;
+  if (bvalue === "") {
+    alert("Enter a value !");
+  } else {
+    const age = getAge(bvalue);
+    result.innerText = age;
+  }
+}
+function getAge(bvalue) {
+  const currentdate = new Date();
+  const bdate = new Date(bvalue);
+  let age = currentdate.getFullYear() - bdate.getFullYear();
+  let month = currentdate.getMonth() - bdate.getMonth();
+
+  if (month < 0 || (month === 0 && currentdate.getDate() < bdate.getDate())) {
+    age--;
+  }
+  return age;
+}
+btn.addEventListener("click", calculateAge);
